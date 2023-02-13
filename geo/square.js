e479cce296021f75e4ee76ac8c044e95a5c0d30a
rkvilena <rwad0711@gmail.com>
2023-02-13 18:51:13 +0700

    [New Feature] Draw Square

--- geo/square.js
+++ geo/square.js
@@ -6,5 +6,73 @@ class Square extends Geometry {
     
     addCanvasListener() {
         // Implement click (drag-drop) behavior
+        let isDown = false;
+        let initPoint = (0.0, 0.0);
+
+        const canvas = document.getElementById("glcanvas");
+        canvas.addEventListener("mousedown", (event) => {
+            isDown = true;
+    
+            if (!this.isDrawn) {
+                initPoint = getMousePos(canvas, event);
+            }
+        }, false);
+
+        canvas.addEventListener("mousemove", (event) => {
+            if (!this.isDrawn && isDown) {   
+                const pos = getMousePos(canvas, event);
+                this.createSquareObject(initPoint, pos, false);
+            }
+        }, false);
+
+        canvas.addEventListener("mouseup", (event) => {
+            isDown = false;
+            
+            if (!this.isDrawn) {
+                const pos = getMousePos(canvas, event);
+                this.createSquareObject(initPoint, pos, true);
+                this.isDrawn = true;
+            }
+        }, false);
+    }
+
+    createSquareObject (initPoint, pos, isFinal){
+        const { r, g, b } = this.params?.color;
+        let sqlen = Math.abs(pos.x - initPoint.x);
+        let posXsmaller = pos.x < initPoint.x;
+        let posYsmaller = pos.y < initPoint.y;
+        let xsubs, ysubs;
+
+        if (posXsmaller){
+            if (posYsmaller) {
+                xsubs = initPoint.x - sqlen;
+                ysubs = initPoint.y - sqlen;
+            } else {
+                xsubs = initPoint.x - sqlen;
+                ysubs = initPoint.y + sqlen;
+            }
+        }
+        else {
+            if (posYsmaller) {
+                xsubs = initPoint.x + sqlen;
+                ysubs = initPoint.y - sqlen;
+
+            }
+            else{
+                xsubs = initPoint.x + sqlen;
+                ysubs = initPoint.y + sqlen;
+            }
+        }
+        this.vertices = [
+            initPoint.x, initPoint.y, r, g, b,
+            xsubs, initPoint.y, r, g, b,
+            xsubs, ysubs, r, g, b,
+            xsubs, ysubs, r, g, b,
+            initPoint.x, ysubs, r, g, b,
+            initPoint.x, initPoint.y, r, g, b
+        ];
+        
+        this.vertices = pixelToClip(this.vertices);
+        this.transformAndDrawObject(isFinal);
     }
 }
